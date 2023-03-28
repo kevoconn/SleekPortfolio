dd5954748a57ac9e95c7692327795a1524182937
github_kevoconn <kcoconnor84@gmail.com>
2023-03-28 14:39:05 -0500

    added components

---
+++ src/resumeData.js
@@ -0,0 +1,143 @@
+import WorkDayScheduler from "./assets/images/WorkDayScheduler.png"
+import TomSkilling from "./assets/images/TomSkilling.png";
+import Takez from "./assets/images/TakezTakeaway.png";
+import Attak from "./assets/images/AttakTracker.png";
+
+
+let resumeData = {
+  imagebaseurl: "https://github.com/kevoconn",
+  name: "Kevin O'Connor",
+  role: "Frontend Developer",
+  email: " kevin.oconnor@northwestern.edu",
+  github: " Kevoconn",
+  linkedinId: "kevin-o-connor-251424270",
+  roleDescription: "A Long Time Ago, In a Galaxy Far, Far Away... I would write a witty description of myself here, but I'm not sure what to say. I'm a full stack web developer with a passion for learning and creating. I'm currently working on a few projects, and I'm always looking for new opportunities to learn and grow.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/kevin-o-connor-251424270/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/kevoconn",
+      className: "fa fa-github",
+    },
+    {
+      name: "Twitter",
+      url: "https://urldefense.com/v3/__https://twitter.com/TalMcgurlagher?t=Rxll6U3eEiKuHM82YNRZqg&s=09__;!!Dq0X2DkFhyF93HkjWTBQKhk!SZ7ioar1pMazlx6l5FDnnxedb7nJXJiM-UQWk_hq5AOUg1FsAMkBWLHMpkKgxAXe9h-bs-hOWKfdHluxViEFn4cYtMX6Zx0$",
+      className: "fa fa-twitter",
+    },
+  ],
+  aboutme: "I am recent bootcamp graduate class of 2023 (WOO-HOO!) and a wannabe junior full-stack developer in Chicago. I am currently employed as a Stationary Engineer but am looking to make the jump to my passion, Web Development.",
+
+  address: "Chicago, IL",
+  website: "kevin.oconnor@northwestern.edu",
+  education: [
+    {
+      UniversityName: "Northwestern University Bootcamp",
+      specialization: "Full Stack Web Development",
+      MonthOfPassing: "April",
+      YearOfPassing: "2023",
+     
+    },
+    {
+      UniversityName: "Triton Community College",
+      specialization: "Building Engineering",
+      MonthOfPassing: "May",
+      YearOfPassing: "2012",
+      Achievements: "Boiler Operator Liscense",
+    },
+    {
+      UniversityName: "University of Illinois at Chicago",
+      specialization: "Criminal Justice",
+      MonthOfPassing: "May",
+      YearOfPassing: "2008",
+      
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Northwestern University",
+      specialization: "Boiler Operator",
+      MonthOfLeaving: "Current",
+      YearOfLeaving: "2023",
+    },
+    {
+      CompanyName: "3701 W. Lake St.",
+      specialization: "Apprentice Stationary Engineer",
+      MonthOfLeaving: "May",
+      YearOfLeaving: "2013",
+     
+    },
+  ],
+  // skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS3",
+    },
+    {
+      skillname: "Reactjs",
+    },
+    {
+      skillname: "Nodejs",
+    },
+    {
+      skillname: "MongoDB",
+    },
+    {
+      skillname: "MySQL",
+    },
+    {
+      skillname: "Express",
+    },
+    {
+      skillname: "GraphQL",
+    },
+    {
+      skillname: "JavaScript ES6+",
+    },
+    {
+      skillname: "Tailwind",
+    },
+    {
+      skillname: "Bootstrap",
+    },
+    {
+      skillname: "JQuery",
+    },
+    {
+      skillname: "GitHub",
+    },
+  ],
+
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: WorkDayScheduler,
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: TomSkilling,
+      
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: Takez,
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: Attak,
+    },
+  ],
+
+};
+
+export default resumeData;
